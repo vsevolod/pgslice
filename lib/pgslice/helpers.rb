bd1dc992b88faeb45b4d63d8f678f0cfe203078a
Everton Silva <everton2x4@gmail.com>
2023-07-04 18:24:08 -0300

    feat: Add support to weekly partitions

--- lib/pgslice/helpers.rb
+++ lib/pgslice/helpers.rb
@@ -3,8 +3,11 @@ module PgSlice
     SQL_FORMAT = {
       day: "YYYYMMDD",
       month: "YYYYMM",
+      week: "YYYYWW",
       year: "YYYY"
-    }
+    }.freeze
+
+    DAYS_IN_WEEK = 7
 
     protected
 
@@ -119,6 +122,8 @@ module PgSlice
       case period.to_sym
       when :day
         "%Y%m%d"
+      when :week
+        "%G%V"
       when :month
         "%Y%m"
       else
@@ -135,6 +140,9 @@ module PgSlice
       case period.to_sym
       when :day
         date
+      when :week
+        now = Date.today
+        now - (now.wday - 1) % 7
       when :month
         Date.new(date.year, date.month)
       else
@@ -155,6 +163,8 @@ module PgSlice
       case period.to_sym
       when :day
         date.next_day(count)
+      when :week
+        date + (count * DAYS_IN_WEEK)
       when :month
         date.next_month(count)
       else
